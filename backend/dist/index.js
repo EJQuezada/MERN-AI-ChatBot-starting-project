798e04d4ddd5c8347d32d1c4e78ce073a97ebea4
Edgar Quezada <edgar.quezada@gmail.com>
2024-10-30 14:45:40 +0100

    update index.js file

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,4 +1,10 @@
 import express from "express";
 const app = express();
+// GET - 
+// PUT -
+// POST -
+// DELETE -
+app.get("/", (req, res, next) => {
+});
 app.listen(5000, () => console.log("Server Open"));
 //# sourceMappingURL=index.js.map
